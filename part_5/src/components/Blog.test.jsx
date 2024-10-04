64319e660b9f3467d71322ae0bb9715081486767
galzmarc <marco.galzignato@gmail.com>
2024-10-04 18:14:47 +0100

    Exercise 5.14

--- part_5/src/components/Blog.test.jsx
+++ part_5/src/components/Blog.test.jsx
@@ -1,4 +1,8 @@
+// Make a test, which checks that the blog's URL and number of likes are shown
+// when the button controlling the shown details has been clicked.
+
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders blog title and author but not details by default', () => {
@@ -23,4 +27,28 @@ test('renders blog title and author but not details by default', () => {
   // Check that the blog details (url, likes) are not visible
   const detailsElement = screen.queryByTestId('blog-details')
   expect(detailsElement).toBeNull()
+})
+
+test('renders blog URL and likes when the "View" button is clicked', async () => {
+  const blog = {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger Dijkstra',
+    url: 'https://example.com',
+    likes: 10,
+    user: {
+      name: 'Test User'
+    }
+  }
+
+  // Render the Blog component
+  render(<Blog blog={blog} />)
+
+  // Simulate clicking the "View" button using userEvent
+  const button = screen.getByText('View')
+  await userEvent.click(button)
+
+  // Check that the blog URL and likes are displayed
+  const detailsElement = screen.getByTestId('blog-details')
+  expect(detailsElement).toHaveTextContent('https://example.com')
+  expect(detailsElement).toHaveTextContent('Likes: 10')
 })
