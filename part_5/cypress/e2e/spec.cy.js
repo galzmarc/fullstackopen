f338a1d79c839d6e5bcd2d1435c825b0307269fa
galzmarc <marco.galzignato@gmail.com>
2024-10-10 19:25:12 +0100

    Exercise 5.19

--- part_5/cypress/e2e/spec.cy.js
+++ part_5/cypress/e2e/spec.cy.js
@@ -3,12 +3,12 @@ describe('Blog app', function() {
     // empty the db here
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
     // create a user for the backend here
-    const user = {
-      name: 'Test User',
-      username: 'testuser',
-      password: 'password'
-    }
-    cy.request('POST', 'http://localhost:3003/api/users/', user)
+    // const user = {
+    //   name: 'Test User',
+    //   username: 'testuser',
+    //   password: 'password'
+    // }
+    // cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:5173')
   })
 
@@ -31,6 +31,23 @@ describe('Blog app', function() {
       cy.get('#password').type('wrong')
       cy.get('#login-button').click()
       cy.contains('Wrong username or password')
+    })
+  })
+
+  describe('When logged in', function() {
+    beforeEach(function() {
+      cy.contains('Log in').click()
+      cy.get('#username').type('admin')
+      cy.get('#password').type('password')
+      cy.get('#login-button').click()
+    })
+
+    it('A blog can be created', function() {
+      cy.contains('new blog').click()
+      cy.get('#title').type('a test blog')
+      cy.get('#author').type('cypress')
+      cy.contains('create').click()
+      cy.contains('a test blog by cypress')
     })
   })
 
