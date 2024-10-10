1677b01461d2049f2a494d777caac276bfb934ed
galzmarc <marco.galzignato@gmail.com>
2024-10-10 18:55:11 +0100

    Exercise 5.18

--- part_5/cypress/e2e/spec.cy.js
+++ part_5/cypress/e2e/spec.cy.js
@@ -1,14 +1,37 @@
 describe('Blog app', function() {
   beforeEach(function() {
+    // empty the db here
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    // create a user for the backend here
+    const user = {
+      name: 'Test User',
+      username: 'testuser',
+      password: 'password'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:5173')
   })
 
-  it('login form is shown', function() {
-    cy.contains('Log in').click()
-    cy.get('#username').type('admin')
-    cy.get('#password').type('password')
-    cy.get('#login-button').click()
-    cy.contains('admin logged in')
+  it('Login form is shown', function() {
+    cy.contains('Log in')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.contains('Log in').click()
+      cy.get('#username').type('admin')
+      cy.get('#password').type('password')
+      cy.get('#login-button').click()
+      cy.contains('admin logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.contains('Log in').click()
+      cy.get('#username').type('admin')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+      cy.contains('Wrong username or password')
+    })
   })
 
 })
