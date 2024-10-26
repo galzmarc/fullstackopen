abd07b6177c5f6dcf9aba09dd01dc74d2806d659
galzmarc <marco.galzignato@gmail.com>
2024-10-26 17:26:17 +0100

    Exercise 6.22

--- part_6/query-anecdotes-main/src/App.jsx
+++ part_6/query-anecdotes-main/src/App.jsx
@@ -1,10 +1,15 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { getAnecdotes, updateAnecdote } from './requests';
 
+import { useContext } from 'react'
+import NotificationContext from './components/NotificationContext'
+
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 const App = () => {
+
+  const [notification, dispatch] = useContext(NotificationContext)
 
   const queryClient = useQueryClient()
 
@@ -14,8 +19,15 @@ const App = () => {
       queryClient.invalidateQueries({ queryKey: ['anecdotes'] })
     }
   })
+
   const handleVote = (anecdote) => {
     updateAnecdoteMutation.mutate({...anecdote, votes: anecdote.votes + 1})
+    // Dispatch the notification message
+    dispatch({ type: 'SET', payload: `anecdote "${anecdote.content}" voted` })
+    // Clear the notification after a 5 seconds delay
+    setTimeout(() => {
+      dispatch({ type: 'CLEAR' })
+    }, 5000)
   }
 
   const result = useQuery({
@@ -23,7 +35,7 @@ const App = () => {
     queryFn: getAnecdotes,
     retry: 1,
   })
-  console.log(JSON.parse(JSON.stringify(result)))
+  // console.log(JSON.parse(JSON.stringify(result)))
 
   if (result.isError) {
     return <div>anecdote service not available due to problems in server</div>
